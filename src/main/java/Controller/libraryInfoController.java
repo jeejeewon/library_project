32c3e3712faacbe0b7201d5b73a43a497482726b
hyeming-way <gpal7355@naver.com>
2025-04-28 23:14:18 +0900

    시설 관련 컨트롤러 doGet, doPost, doHandle 작성

--- src/main/java/Controller/libraryInfoController.java
+++ src/main/java/Controller/libraryInfoController.java
@@ -1,6 +1,46 @@
 package Controller;
 
-//시설 안내 컨트롤러
-public class libraryInfoController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//시설 안내 컨트롤러
+@WebServlet("/info/*")
+public class libraryInfoController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		String action = request.getPathInfo();
+		
+		System.out.println("action: " + action);
+		
+		
+	
+		
+		
+	}//doHandle
+
+	
+	
+	
+	
+	
 }
--- src/main/java/Controller/roomReserveController.java
+++ src/main/java/Controller/roomReserveController.java
@@ -1,6 +1,43 @@
 package Controller;
 
-//시설 예약 컨트롤러
-public class roomReserveController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//시설 예약 컨트롤러
+@WebServlet("/reserve/*")
+public class roomReserveController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		String action = request.getPathInfo();
+		
+		System.out.println("action: " + action);
+		
+		
+		
+		
+		
+		
+	}//doHandle
+	
+	
 }
